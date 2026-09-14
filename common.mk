kaffuchino233/device_xiaomi_sm8450-common

sm8450-common: Opt out of speaker_layout_channel_mask field
Required for ABI compatibility with audio HAL compiled before 15 QPR2.

Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
Change-Id: Ic910f0634ee0707c21dad77f5264fd89fb33f1c0
Signed-off-by: Ghosuto <clash.raja10@gmail.com>

common.mk
@@ -80,6 +80,7 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libvolumelistener
 
+$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 $(call soong_config_set, android_hardware_audio, run_64bit, true)
 
 $(foreach sku, taro diwali cape ukee parrot, \

afkyoung/device_xiaomi_sm8450_common

common.mk
@@ -80,6 +80,7 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libvolumelistener
 
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 $(call soong_config_set, android_hardware_audio, run_64bit, true)
 
 $(foreach sku, taro diwali cape ukee parrot, \